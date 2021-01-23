97c02b9370a753ccb2f5524e0dd790d8d9c4e351
Miguel Ramos <miguelrb@microsoft.com>
2021-01-22 21:03:12 -0800

    Example of how you can center the Window in Win32

--- src/Build2020Demo/DemoBuildCs/DemoBuildCs/DemoBuildCs/App.xaml.cs
+++ src/Build2020Demo/DemoBuildCs/DemoBuildCs/DemoBuildCs/App.xaml.cs
@@ -35,7 +35,9 @@ namespace DemoBuildCs
             // The Window object doesn't have Width and Height properties in WInUI 3 Desktop yet.
             // To set the Width and Height, you can use the Win32 API SetWindowPos.
             // Note, you should apply the DPI scale factor if you are thinking of dpi instead of pixels.
-            SetWindowSize(m_windowHandle, 800, 600);
+            //SetWindowSize(m_windowHandle, 800, 600);
+
+            CenterWindow(m_windowHandle, 800, 600);
         }
 
         private void SetWindowSize(IntPtr hwnd, int width, int height)
@@ -50,9 +52,35 @@ namespace DemoBuildCs
                                         PInvoke.User32.SetWindowPosFlags.SWP_NOMOVE);
         }
 
+        private void CenterWindow(IntPtr hnwd, int width, int height)
+        {
+            IntPtr hwndDesktop = PInvoke.User32.GetDesktopWindow();
+
+            PInvoke.RECT rectParent;
+            PInvoke.User32.GetClientRect(hwndDesktop, out rectParent);
+
+            var dpi = PInvoke.User32.GetDpiForWindow(hwndDesktop);
+            float scalingFactor = (float)dpi / 96;
+
+            width = (int)(width * scalingFactor);
+            height = (int)(height * scalingFactor);
+
+            rectParent.left = (rectParent.right / 2) - (width / 2);
+            rectParent.top = (rectParent.bottom / 2) - (height / 2);
+
+            PInvoke.User32.SetWindowPos(hnwd, PInvoke.User32.SpecialWindowHandles.HWND_TOP,
+                                        rectParent.left, rectParent.top, width, height,
+                                        PInvoke.User32.SetWindowPosFlags.SWP_SHOWWINDOW);
+
+        }
+
+
         private Window m_window;
         private IntPtr m_windowHandle;
         public IntPtr WindowHandle { get { return m_windowHandle; } }
+
+        [DllImport("user32.dll", ExactSpelling = true, CharSet = CharSet.Auto)]
+        public static extern IntPtr GetParent(IntPtr hWnd);
 
         [ComImport]
         [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
