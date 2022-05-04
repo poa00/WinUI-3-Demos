1bec3c49f7b6ec6f27f8ab39cd2456a7208ec003
Miguel Ramos <miguelrb@microsoft.com>
2022-05-03 21:58:14 -0700

    Addin Mica

--- src/Build2022Demo/BasicPhotoViewer/MainWindow.xaml.cs
+++ src/Build2022Demo/BasicPhotoViewer/MainWindow.xaml.cs
@@ -1,33 +1,32 @@
-﻿using Microsoft.UI.Xaml;
+﻿using Microsoft.UI.Composition;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using System;
-using System.IO;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Threading.Tasks;
-using Windows.ApplicationModel;
-using Windows.Storage;
-using Windows.Storage.Pickers;
-using Windows.Storage.Search;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Media.Imaging;
 using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Composition;
+using Microsoft.UI.Xaml.Media.Imaging;
+using System;
 using System.Numerics;
+using Windows.Storage.Pickers;
+
+//Mica
+using WinRT; // required to support Window.As<ICompositionSupportsSystemBackdrop>()
 
 namespace PhotoViewer
 {
     public sealed partial class MainWindow : Window
     {
-
         private string _folder;
         public ImagesRepository ImagesRepository { get; } = new ImagesRepository();
 
         public MainWindow()
         {
             this.InitializeComponent();
-            Title = "Simple Photo Viewer";
+
+            this.ExtendsContentIntoTitleBar = true;
+            this.SetTitleBar(CustomTitleBar);
             
+            TrySetMicaBackdrop();
+            Title = "Simple Photo Viewer";
+
             string _folder = "C:\\Users\\migue\\source\\repos\\WinUI-3-Demos\\src\\Build2022Demo\\Photos";
 
             LoadImages(_folder);
@@ -114,8 +113,62 @@ namespace PhotoViewer
                 CloseButtonText = "Ok"
             };
             //Explain XamlRoot concept. You can get the XamlRoot of any UIElement
-            dialog.XamlRoot =  this.Content.XamlRoot;
+            dialog.XamlRoot = this.Content.XamlRoot;
             await dialog.ShowAsync();
         }
+        #region Mica
+        WindowsSystemDispatcherQueueHelper m_wsdqHelper; // See separate sample below for implementation
+        Microsoft.UI.Composition.SystemBackdrops.MicaController m_micaController;
+        Microsoft.UI.Composition.SystemBackdrops.SystemBackdropConfiguration m_configurationSource;
+        bool TrySetMicaBackdrop()
+        {
+            if (Microsoft.UI.Composition.SystemBackdrops.MicaController.IsSupported())
+            {
+                m_wsdqHelper = new WindowsSystemDispatcherQueueHelper();
+                m_wsdqHelper.EnsureWindowsSystemDispatcherQueueController();
+
+                // Hooking up the policy object
+                m_configurationSource = new Microsoft.UI.Composition.SystemBackdrops.SystemBackdropConfiguration();
+                this.Activated += Window_Activated;
+                this.Closed += Window_Closed;
+
+                // Initial configuration state.
+                m_configurationSource.IsInputActive = true;
+                switch (((FrameworkElement)this.Content).ActualTheme)
+                {
+                    case ElementTheme.Dark: m_configurationSource.Theme = Microsoft.UI.Composition.SystemBackdrops.SystemBackdropTheme.Dark; break;
+                    case ElementTheme.Light: m_configurationSource.Theme = Microsoft.UI.Composition.SystemBackdrops.SystemBackdropTheme.Light; break;
+                    case ElementTheme.Default: m_configurationSource.Theme = Microsoft.UI.Composition.SystemBackdrops.SystemBackdropTheme.Default; break;
+                }
+
+                m_micaController = new Microsoft.UI.Composition.SystemBackdrops.MicaController();
+
+                // Enable the system backdrop.
+                // Note: Be sure to have "using WinRT;" to support the Window.As<...>() call.
+                m_micaController.AddSystemBackdropTarget(this.As<Microsoft.UI.Composition.ICompositionSupportsSystemBackdrop>());
+                m_micaController.SetSystemBackdropConfiguration(m_configurationSource);
+                return true; // succeeded
+            }
+
+            return false; // Mica is not supported on this system
+        }
+        private void Window_Activated(object sender, WindowActivatedEventArgs args)
+        {
+            m_configurationSource.IsInputActive = args.WindowActivationState != WindowActivationState.Deactivated;
+        }
+
+        private void Window_Closed(object sender, WindowEventArgs args)
+        {
+            // Make sure any Mica/Acrylic controller is disposed so it doesn't try to
+            // use this closed window.
+            if (m_micaController != null)
+            {
+                m_micaController.Dispose();
+                m_micaController = null;
+            }
+            this.Activated -= Window_Activated;
+            m_configurationSource = null;
+        }
+        #endregion
     }
 }
